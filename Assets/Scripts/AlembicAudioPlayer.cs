0993c8674fcae146bb32a91cc93fe1332eb2d417
n1ckfg <nick@fox-gieg.com>
2018-03-28 00:11:37 -0400

    first

---
+++ Assets/Scripts/AlembicAudioPlayer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UTJ.Alembic;
+
+public class AlembicAudioPlayer : MonoBehaviour {
+
+    public AudioSource audio;
+    public AlembicStreamPlayer alembic;
+
+	void Update () {
+        alembic.currentTime = audio.time;
+	}
+}
